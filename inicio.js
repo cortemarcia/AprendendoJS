cb7b45e97d0980407bbd6464503d4a5d29e853e4
marcia <cortemarcia80@gmail.com>
2019-09-23 21:38:11 -0300

    Aprendendo JS

---
+++ inicio.js
@@ -0,0 +1,22 @@
+var frase = "Olá, Mundo!";
+alert (frase);
+
+var x = 1 ;
+var y = 9 ;
+let soma = (x + y);
+
+console.log (soma);
+
+var ano= prompt ("Em que  ano voce nasceu");
+var atual= 2019;
+var idade= (atual -ano);
+alert( idade);
+
+
+var um= parseInt (prompt ("Digite o primeiro numero"));
+var dois= parseInt (prompt ("Digite o segundo numero"));
+var tres= parseInt(prompt ("Digite o terceiro numero"));
+let soma= (um + dois + tres);
+let media=(soma /3);
+alert (media);
+
