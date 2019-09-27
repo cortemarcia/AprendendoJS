884d6f0cc3f5cf25620a92d76ecf7640d1752fe4
marcia <marciatainan@hotmail.com>
2019-09-27 15:30:10 -0300

    Retirado // das funcoes

--- 092619.js
+++ 092619.js
@@ -1,21 +1,20 @@
-// // 1. Crie uma função que dado um número seja retornado o número reverso. Ex.: dado 1234, devolve 4321.
+﻿// // 1. Crie uma função que dado um número seja retornado o número reverso. Ex.: dado 1234, devolve 4321.
 
-// function inverterNumero(numero){
-//   return numero.toString().split("").reverse().join("");  
-// }
+function inverterNumero(numero){
+  return numero.toString().split("").reverse().join("");  
+}
 
-// console.log (inverterNumero("1234"));
+ console.log (inverterNumero("1234"));
 
 // // ________________________________________________________________________
 
 
 // 2. Crie uma função que recebe uma palavra e ordena suas letras em ordem alfabética.
 
-// function ordenarpalavra(palavra){
-//   return palavra.toString().split("").sort().join("");
-// }
-
-// console.log(ordenarpalavra("Marcia"));
+function ordenarpalavra(palavra){
+  return palavra.toString().split("").sort().join("");
+}
+ console.log(ordenarpalavra("Marcia"));
 
 // __________________________________________________________________________________
 
@@ -23,25 +22,24 @@
 
   
 
-// function pegaIniciais(nomeSobrenome){
-//   let letra1 = nomeSobrenome.substr(0,1);
+function pegaIniciais(nomeSobrenome){
+ let letra1 = nomeSobrenome.substr(0,1);
   
-//   let posicaoEspaco = nomeSobrenome.indexOf(" ");
-//   let letra2 = nomeSobrenome.substr(posicaoEspaco+1, 1);
-  
-//   return letra1 + letra2;
-  
-// }
-//  console.log (pegaIniciais("Marcia CORTE"));
+ let posicaoEspaco = nomeSobrenome.indexOf(" ");
+ let letra2 = nomeSobrenome.substr(posicaoEspaco+1, 1);
+ return letra1 + letra2;
+   
+}
+ console.log (pegaIniciais("Marcia CORTE"));
 
 ___________________________________________________________
 
 // 4. Crie uma função que receba a quantidade de bilhetes vendidos em um concurso e sorteie um bilhete.
 
 
-// function qtdBilhetes(min, max){
-//   return Math.floor(Math.random() * max + 1);
+function qtdBilhetes(min, max){
+ return Math.floor(Math.random() * max + 1);
   
-// }
+}
 
-// console.log (qtdBilhetes(10, 70));
+console.log (qtdBilhetes(10, 70));
