e1b23ecf44da1fe99851f0c00b66f52198bd3b72
marcia <cortemarcia80@gmail.com>
2019-09-30 21:10:00 -0300

    Adicionando atividade

---
+++ 092719.js
@@ -0,0 +1,61 @@
+// 6. Baseado na tabela abaixo, escreva um algoritmo que leia o código do item adquirido pelo consumidor e a quantidade, calculando e mostrando o valor a pagar. A mensagem de retorno deve conter o produto, a quantidade e o valor total a pagar.
+
+
+// function fazerPedido (codigo, quantidade){
+//     switch(codigo) {
+//         case 100:
+//              return "Vc esta comprando Cachorro quente " + "por " + 1.70* quantidade ;
+
+//         case 101:
+//              return "Vc esta comprando Bauru Simples " + "por " + 2.30* quantidade ;
+
+//         case 102:
+//              return "Vc esta comprando Bauru com ovo " + "por " + 2.60* quantidade ;
+
+//         case 103:
+//              return "Vc esta comprando Hamburguer " + "por " + 2.40* quantidade ; 
+             
+//         case 104:
+//             return "Vc esta comprando Cheeseburguer " + "por " + 2.40* quantidade ;
+            
+//         case 105:
+//              return "Vc esta comprando Refrigerante " + "por " + 1.00* quantidade ;    
+
+//          default:
+//      return "Não tem esse produto";
+//     }
+     
+// }
+//  console.log(fazerPedido( 105, 3));
+
+
+//  _______________________________________________________________________________________________________
+
+
+// 7. Escreva um código que receba dois número e determine qual o maior entre eles.
+
+function calcularnumero(num1, num2){
+    if (num1 > num2 ) 
+     return 'O número '+ num1 + " é maior.";
+    
+    else if (num1 === num2)
+     return 'Nenhum é maior';
+     
+    else (num1 < num2)
+     return 'O número ' + num2 + " é maior." ;
+
+}
+
+console.log (calcularnumero( 100, 70));
+
+// _________________________________________________________________________________________________________
+
+8. Escreva um código que receba um número (1-7) e devolva o dia da semana correspondente.
+
+
+
+
+
+ 
+ 
+ 
